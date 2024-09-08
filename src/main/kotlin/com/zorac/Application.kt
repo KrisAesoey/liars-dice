39bb409d568fcff18f9153e7da2253c0bf345abd
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2024-09-08 12:54:59 +0200

    🎉 Initialize project

---
+++ src/main/kotlin/com/zorac/Application.kt
@@ -0,0 +1,19 @@
+package com.zorac
+
+import com.zorac.plugins.configureRouting
+import com.zorac.plugins.configureSerialization
+import com.zorac.plugins.configureSockets
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureRouting()
+}
